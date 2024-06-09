8bdb26eb9b9b1aa16833b92f7f02b86d280b71dc
rebecca554owen <rebecca554owen@gmail.com>
2024-06-09 21:02:55 +0800

    Update snell.sh

--- snell.sh
+++ snell.sh
@@ -1,46 +1,50 @@
 #!/bin/sh
 
-# docker run -e PSK=<your_psk_here> -p<your_host_port_here>:9102 --restart unless-stopped -d snell-server:latest
+BIN="/opt/snell/snell-server"
+CONF="/opt/snell/snell-server.conf"
+PORT=${PORT:-2024}
 
-BIN="/usr/bin/snell-server"
-CONF="/etc/snell-server.conf"
+# 提示用户如何使用此镜像
+echo "要运行此镜像，请使用以下命令："
+echo "docker run -e PSK=<your_psk_here> -e PORT=<your_port_here> --restart unless-stopped -d rebecca554owen/snell:latest"
+echo "例如："
+echo "docker run -e PSK=your_psk -e PORT=2024 --restart unless-stopped -d snell-server:latest"
 
-# Download snell binary
-
+# 下载 snell 二进制文件
 download_bin() {
-wget --no-check-certificate -O snell.zip $SNELL_URL
-unzip snell.zip
-rm -f snell.zip
-chmod +x snell-server
-mv snell-server /usr/bin/
+    wget --no-check-certificate -O snell.zip $SNELL_URL
+    unzip snell.zip
+    rm -f snell.zip
+    chmod +x snell-server
+    mv snell-server /opt/snell/
 }
 
-# reuse existing config when the container restarts
-
+# 容器重启时重新使用现有配置
 run_bin() {
     ${BIN} --version
     ${BIN} -c ${CONF}
 }
+
 if [ -f ${BIN} ]; then
-    echo "Found existing binary..."
+    echo "找到已有的二进制文件..."
     run_bin
 fi
 
 if [ -f ${CONF} ]; then
-    echo "Found existing config..."
+    echo "找到已有的配置文件..."
     download_bin
     run_bin
 else
     if [ -z ${PSK} ]; then
         PSK=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 16)
-        echo "Using generated PSK: ${PSK}"
+        echo "使用生成的 PSK: ${PSK}"
     else
-        echo "Using predefined PSK: ${PSK}"
+        echo "使用预定义的 PSK: ${PSK}"
     fi
-    echo "Generating new config..."
+    echo "生成新的配置文件..."
     echo "[Snell Server]" >> ${CONF}
     echo "interface = 0.0.0.0" >> ${CONF}
-    echo "port = 6160" >> ${CONF}
+    echo "port = ${PORT}" >> ${CONF}
     echo "psk = ${PSK}" >> ${CONF}
     run_bin
 fi
